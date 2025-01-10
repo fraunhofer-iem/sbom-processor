81cbfa65aaa0c8a42173cc5342f78a7841446fed
Jan-Niclas Struewer <j.n.struewer@gmail.com>
2025-01-10 16:26:23 +0100

    chore: removed dead code

--- internal/json/json.go
+++ internal/json/json.go
@@ -37,23 +37,6 @@ func CollectJsonFiles(p string) ([]string, error) {
 			paths = append(paths, p)
 		}
 
-		// filepath.WalkDir(p, func(path string, d fs.DirEntry, err error) error {
-		// 	if err != nil {
-		// 		return err
-		// 	}
-
-		// 	if d.IsDir() {
-		// 		return nil
-		// 	}
-
-		// 	if filepath.Ext(path) != ".json" {
-		// 		return nil
-		// 	}
-
-		// 	paths = append(paths, path)
-
-		// 	return nil
-		// })
 	} else {
 		if filepath.Ext(p) != ".json" {
 			return paths, nil
